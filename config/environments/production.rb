41c8121b804c794310e8e53450833ca7cb1ce8b6
TYLER FEDRIZZI <TYLER.FEDRIZZI@GMAIL.COM>
2017-12-04 08:54:13 -0500

    Fixing logo again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.cache_classes = true
   
   config.serve_static_files = true
-  config.assets.compile = true
+ 
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -41,7 +41,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 

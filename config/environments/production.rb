c614731b87e10a5cce4b39fe7ebc919d50b09df1
TYLER FEDRIZZI <TYLER.FEDRIZZI@GMAIL.COM>
2017-12-10 16:09:10 -0500

    fixed environment errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.autoload_paths += %W(#{config.root}/lib)
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 

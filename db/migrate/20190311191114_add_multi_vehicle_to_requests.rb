bd9650fc52c0e093b57c70ddf6964d3e747fa1df
Tyler Fedrizzi <tyler.fedrizzi@gmail.com>
2019-03-12 11:58:41 -0400

    Update migration for removing extra columns from requests

--- db/migrate/20190311191114_add_multi_vehicle_to_requests.rb
+++ db/migrate/20190311191114_add_multi_vehicle_to_requests.rb
@@ -2,8 +2,6 @@ class AddMultiVehicleToRequests < ActiveRecord::Migration[5.1]
   def change
     add_column :requests, :multi_vehicle, :boolean, default: false
     remove_column :requests, :special_requets, :text
-    remove_column :requests, :poc, :string
-    remove_column :requests, :mechanic_id, :integer
     remove_column :requests, :tracker_id, :integer
     remove_column :requests, :checklist_numb, :integer
   end
